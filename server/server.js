54f2624f67c9dcabac749f6abfb79db305d26298
Bhavy Chaudhary <=>
2026-07-15 23:44:10 +0530

    Completed backend deployment

--- server/server.js
+++ server/server.js
@@ -1,31 +1,11 @@
-require("dotenv").config();
-
 const app = require("./src/app");
-const mongoose = require("mongoose");
+require("dotenv").config();
+const connectDB = require("./src/db/db.connect");
 
-const connectDB = async () => {
-  console.log("Connecting to MongoDB...");
-
-  try {
-    console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
-
-    await mongoose.connect(process.env.MONGO_URI);
-
-    console.log("✅ MongoDB connected");
-  } catch (error) {
-    console.error("❌ Error connecting to MongoDB:", error);
-    process.exit(1);
-  }
-};
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-async function startServer() {
-  await connectDB();
-
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
-
-startServer();
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});   
